32958dd68f4de00edd784e8a888a0765b1d8830b
Pierre Mortier <pierremortier1@gmail.com>
2021-10-06 12:10:25 +0200

    liens connexion-inscription et accueil

--- src/Components/Connexion.js
+++ src/Components/Connexion.js
@@ -2,7 +2,8 @@ import React, { useState } from 'react'
 import '../App.css'
 import { auth } from '../firebase'
 import {signInWithEmailAndPassword} from 'firebase/auth'
-import { Link, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import FontAwesome from 'react-fontawesome';
 
 function Connexion() {
   const [email, setEmail] = useState('')
@@ -26,32 +27,35 @@ function Connexion() {
   }
 
   return (
-    <form onSubmit={send}>
+    <div>
+      <Link to="/"><FontAwesome name="times"/></Link>
       <h1>Connexion</h1>
-      <div>
-        <label htmlFor="email">Votre email:</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Votre mot de passe:</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-        />
-      </div>
-      {error && <p>{error}</p>}
-      <div>
-        <button type="submit">Connexion</button>
-      </div>
+      <form onSubmit={send}>
+        <div>
+          <label htmlFor="email">Votre email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Votre mot de passe:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+        </div>
+        {error && <p>{error}</p>}
+        <div>
+          <button type="submit">Connexion</button>
+        </div>
+      </form>
       <Link to="/inscription">Créer un compte</Link>
-    </form>
+    </div>
   )
 }
 
--- src/Components/Inscription.js
+++ src/Components/Inscription.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import '../App.css'
 import { auth } from '../firebase'
 import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { Link } from 'react-router-dom';
+import FontAwesome from 'react-fontawesome';
 
 function Inscription() {
   const [email, setEmail] = useState('')
@@ -20,30 +22,33 @@ function Inscription() {
   }
 
   return (
-    <form onSubmit={send}>
+    <div>
+      <Link to="/"><FontAwesome name="times"/></Link>
       <h1>Inscription</h1>
-      <div>
-        <label htmlFor="email">Votre email:</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Votre mot de passe:</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-        />
-      </div>
-      <div>
-        <button type="submit">Inscription</button>
-      </div>
-    </form>
+      <form onSubmit={send}>
+        <div>
+          <label htmlFor="email">Votre email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Votre mot de passe:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+        </div>
+        <div>
+          <button type="submit">Inscription</button>
+        </div>
+      </form>
+    </div>
   )
 }
 
